8fa80053eddd3792d2115bf13dc087c997a07a80
udwije <udwijewardhana@gmail.com>
2025-05-17 20:06:43 +0530

    structure change

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+# =========================
+# main.py
+# =========================
+
+# main.py
 import sys
-from PyQt5.QtWidgets import QApplication
-from app.gui import MainWindow
+from PyQt6.QtWidgets import QApplication
+from quickytdl.ui.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
