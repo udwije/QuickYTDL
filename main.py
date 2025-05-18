f3e78450cdc0ef0975189e28f8db9e52ed6f349b
udwije <udwijewardhana@gmail.com>
2025-05-18 05:35:16 +0530

    dist update

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from PyQt6.QtWidgets import QApplication
 from PyQt6.QtCore import QLoggingCategory
 from quickytdl.ui.main_window import MainWindow
 from PyQt6.QtGui import QIcon
-import quickytdl.resources_rc  # make sure your .qrc has been compiled
 
 def main():
     # suppress Qt paint/font warnings
@@ -13,7 +12,6 @@ def main():
         "qt.text.font.db=false"
     )
     app = QApplication(sys.argv)
-    #app.setWindowIcon(QIcon(":/QuickYTDL.ico"))  # set taskbar & window icon
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
